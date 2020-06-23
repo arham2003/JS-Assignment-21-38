005de9fc7381dee09c505fe7a9b90bf17a97f028
arham2003 <arhamkx@gmail.com>
2020-06-23 23:21:38 +0500

    done

---
+++ app.js
@@ -0,0 +1,104 @@
+//---- Chapter 21-----\\
+//---task1--\\
+
+// var firstName = prompt("Enter First Name: ", "Arham");
+// var lastName = prompt("Enter Last Name", "khan");
+// var fullName = firstName + " " + lastName;
+// alert("Welcome " + fullName + "!");
+
+//---task2--\\
+
+// var favPhone = prompt("What Is Your Favourite Phone?", "Galaxy S20 Plus");
+// var length = favPhone.length;
+// document.write("My Favourite Phone is: " + favPhone);
+// document.write("<br>");
+// document.write("Length of String: " + length);
+
+//---task3--\\
+
+// var country = "Pakistani";
+// var Char = country.indexOf('n');
+// document.write("String: " + country + "<br>");
+// document.write("Index of 'n' : " + Char);
+
+//---task4--\\
+// var word = "Hello World"
+// var char = word.lastIndexOf('l')
+// document.write("String: " + word + "<br>")
+// document.write("Last index of 'l': " + char)
+
+//---task5--\\
+
+// var country = "Pakistani"
+// var char = country.slice(3, 4)
+// document.write("String: " + country + "<br>")
+// document.write("Character at index 3: " + char)
+
+//---task 6--\\
+
+// var firstName = prompt("Enter First Name: ", "Arham");
+// var lastName = prompt("Enter Last Name", "khan");
+// var fullName = firstName.concat(lastName)
+// alert("Welcome! " + fullName)
+
+//---task7--\\
+
+// var city = "Hyderabad";
+// var firstWord = city.replace("Hyder" , "Islam")
+
+// document.write("City: " + city + "<br>");
+// document.write("After replacement: " + firstWord)
+
+//---task8--\\
+
+// var message = ("Ali and Sami are best friends. They play cricket and football together.");
+// var After = message.replace(/and/g, "&")
+// document.write(After)
+
+//----task 9---\\
+
+// var integerString = "472"
+// var num = Number(integerString)
+// document.write("Value: " + integerString + "<br>" + "Type: string" + "<br>")
+// document.write("Value: " + num + "<br>" + "Type: number")
+
+//---task10--\\
+
+// var userInput = prompt("Enter your response: ", "peanut")
+// var upperCase = userInput.toUpperCase()
+
+// document.write("User input: " + userInput + "<br>")
+// document.write("Upper case: " + upperCase)
+
+//---task 11 --\\
+
+// var userInput = prompt("Enter your response: ", "peanut")
+// function titleCase(string) {
+//     var sentence = string.toLowerCase().split(" ");
+//     for(var i = 0; i< sentence.length; i++){
+//        sentence[i] = sentence[i][0].toUpperCase() + sentence[i].slice(1);
+//     }
+// //  document.write(sentence.join(" "));
+//  return sentence;
+//  }
+//  var Titlecase = titleCase(userInput);
+
+//  document.write("User input: " + userInput + "<br>")
+//  document.write("Title case: " + Titlecase)
+
+//----task 12---\\
+
+// var num = 35.36;
+// var newNum = num.toString();
+// var n = parseInt(num.toString().replace('.', ''));
+// document.write("Number: " + num + "<br>");
+// document.write("Result: " + n);
+
+//---task 13 ----\\
+
+difficult!
+
+//---task 14--\\
+
+var A = ["cake", "apple pie", "cookie", "chips","patties"]
+
